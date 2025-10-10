5dee583025a54a4ce49a1681ca4c8edce13afa22
Daniel <olusakindaniel03@gmail.com>
2025-10-10 17:56:29 +0100

    init: config files

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,5 +9,5 @@ app.use(express.json())
 // Tell the app to use your auth routes for any request to /api/auth
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+    console.log(`Listening on port ${port}`)
 })
